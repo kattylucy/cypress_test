7aeab7024f224f1318a9e4ad3aea549df906835e
Katty Barroso <kattybarroso@ip-192-168-86-30.ec2.internal>
2021-01-13 11:42:57 -0800

    working with then command

---
+++ cypress/integration/QAM/TC05_thenCommand.spec.js
@@ -0,0 +1,36 @@
+describe("Create and mark-unmark as favorite", function(){
+    
+    Cypress.config('pageLoadTimeout', 100000)
+
+    before(function(){
+        cy.SignIn()
+    })
+
+    it('Create a post', function(){
+        cy.get('ul.navbar-nav').children().contains('New Post').click()
+        cy.hash().should('include', '#/editor')
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type('test')
+            cy.get('input').eq(1).type('test')
+            cy.get('textarea').type('description test')
+            cy.get('.btn').contains('Publish Article').should('be.visible').click()
+
+        })
+        cy.url().should('include', 'article')
+    });
+
+    it('Mark/Unmark a post', function(){
+        cy.get('.nav-link').last().click()
+        cy.contains('My Articles').click()
+        cy.get('.ion-heart').first().click()
+        cy.contains('Favorited Articles').click()
+        cy.url().should('include', 'favorites')
+        cy.get('.btn-primary').first().then(($fav) => {
+            const favCount = $fav.text()
+            expect(parseInt(favCount)).to.eq(1)
+        }).click()
+        cy.reload()
+        cy.contains('No articles are here... yet.').should('be.visible')
+        cy.go('back')
+    });
+});
