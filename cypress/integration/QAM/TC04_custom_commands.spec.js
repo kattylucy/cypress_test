e43f0a50706f707f61f24f25dec3d09c1d5ffe9e
Katty Barroso <kattybarroso@ip-192-168-86-30.ec2.internal>
2021-01-13 11:27:23 -0800

    practice adding custom command

---
+++ cypress/integration/QAM/TC04_custom_commands.spec.js
@@ -0,0 +1,30 @@
+describe("Create and mark-unmark as favorite", function(){
+    before(function(){
+        cy.SignIn()
+    })
+
+    it('Create a post', function(){
+        cy.get('ul.navbar-nav').children().contains('New Post').click()
+        cy.hash().should('include', '#/editor')
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type('test')
+            cy.get('input').eq(1).type('test')
+            cy.get('textarea').type('description test')
+            cy.get('.btn').contains('Publish Article').should('be.visible').click()
+
+        })
+        cy.url().should('include', 'article')
+    });
+
+    it('Mark/Unmark a post', function(){
+        cy.get('.nav-link').last().click()
+        cy.contains('My Articles').click()
+        cy.get('.ion-heart').first().click()
+        cy.contains('Favorited Articles').click()
+        cy.url().should('include', 'favorites')
+        cy.get('.ion-heart').first().click()
+        cy.reload()
+        cy.contains('No articles are here... yet.').should('be.visible')
+        cy.go('back')
+    });
+});
