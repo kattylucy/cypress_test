714681b69b633759da95390245f97a88e16a8f95
Katty Barroso <kattybarroso@ip-192-168-86-30.ec2.internal>
2021-01-13 09:42:22 -0800

    first cypress test in login

---
+++ cypress/integration/QAM/TC01_basics.spec.js
@@ -0,0 +1,8 @@
+describe("login", function(){
+    it('Sign in', function(){
+        cy.visit('https://react-redux.realworld.io/#/login')
+        cy.get('input[type="email"]').type('kattybarroso1321@gmail.com')
+        cy.get('input[type="password"]').type('123456')
+        cy.get('.btn').contains('Sign in').click()
+    })
+})
